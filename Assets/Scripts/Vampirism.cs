eeffadc2dcbcb885638e8fd900de710bdb94d054
Vladimir <w112vovan@mail.ru>
2024-03-26 16:25:06 +0500

    2.2.1

--- Assets/Scripts/Vampirism.cs
+++ Assets/Scripts/Vampirism.cs
@@ -1,26 +1,28 @@
-using Game;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vampirism : ISkill
+namespace Game
 {
-    [SerializeField] private ContactFilter2D _contactFilter;
-
-    private float _radius = 3f;
-
-    public override event Action<IDamageble> OnHit;
-
-    private void OnEnable()
+    public class Vampirism : ISkill
     {
-        List<Collider2D> hits = new List<Collider2D>();
-        //Physics2D.OverlapBox(transform.position, _radius, 0, _contactFilter, hits);
-        Physics2D.OverlapCircle(transform.position, _radius, _contactFilter, hits);
+        [SerializeField] private ContactFilter2D _contactFilter;
 
-        foreach (Collider2D hit in hits)
+        private float _radius = 3f;
+
+        public override event Action<IDamageble> OnHit;
+
+        private void OnEnable()
         {
-            if (hit.TryGetComponent<IDamageble>(out IDamageble obj))
-                OnHit?.Invoke(obj);
+            List<Collider2D> hits = new List<Collider2D>();
+            //Physics2D.OverlapBox(transform.position, _radius, 0, _contactFilter, hits);
+            Physics2D.OverlapCircle(transform.position, _radius, _contactFilter, hits);
+
+            foreach (Collider2D hit in hits)
+            {
+                if (hit.TryGetComponent<IDamageble>(out IDamageble obj))
+                    OnHit?.Invoke(obj);
+            }
         }
     }
 }
